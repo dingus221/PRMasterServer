cd1dea6352bbefa7e6b9f952c304d2b7a9db0a08
Bruce Wayne <dingus221@users.noreply.github.com>
2014-10-30 12:20:46 +0400

    Update Program.cs

--- PRMasterServer/Program.cs
+++ PRMasterServer/Program.cs
@@ -6,7 +6,6 @@ using System.Net;
 using System.Threading;
 using System.Linq;
 using System.Collections.Generic;
-//using serverlistretrieve;
 
 
 
@@ -20,41 +19,25 @@ namespace PRMasterServer
     /// To run as a (Civilization 4 Beyond the Sword) NAT Negotiation server:
     /// PRMasterServer.exe +game civ4bts +servers master,natneg
     /// 
+    /// To run as Civilization 4 vanilla, bts, btsjp full gamespy lobby
+    /// PRMasterServer.exe +db logindb.db3 +game civ4bts +servers master,login,cdkey,list,natneg
     /// </summary>
     
 	class Program
 	{
-        //public struct supportedgames { string gamename;}
         
         public static string gameName = null;
-
-
         public static string getGKey(string gname)
         {
-            //make it into array
-            //bool potato = false;
-            string faget = "FAGGET";
-            string[] gamelist = { "civ4", "civ4bts", "civ4btsjp" };
-            string[] GKlist = { "y3D9Hw", "Cs2iIq", "Cs2iIq" };
-            //LogGreen(gamelist.Count);
-            for (int i = 0; i < gamelist.Length; i++) { if (gname == gamelist[i]) { faget=GKlist[i]; } }
-            //  char *strarray[][] = {"hey", "sup", "dogg"};
-            return faget;
+            string f = "Cs2iIq";//if other game comes they get civ4bts response
+            string[] gamelist = { "civ4", "civ4bts", "civ4btsjp","gmtest" };
+            string[] GKlist = { "y3D9Hw", "Cs2iIq", "Cs2iIq", "HA6zkS" };
+            for (int i = 0; i < gamelist.Length; i++) { if (gname == gamelist[i]) { f=GKlist[i]; } }
+            return f;
         }
 
-        public static bool isSupported(string gn) 
-        { 
-            //make it into array
-            bool potato=false;
-            string[] gamelist = { "civ4", "civ4bts", "civ4btsjp" };
-            //LogGreen(gamelist.Count);
-            for (int i = 0; i < gamelist.Length; i++) { LogGreen(gamelist[i]); if (gamelist[i] == gn) potato = true; }
-                //  char *strarray[][] = {"hey", "sup", "dogg"};
-                return potato; 
-        }
 		private static readonly object _lock = new object();
-
-        
+       
 		static void Main(string[] args)
 		{
 			Action<string, string> log = (category, message) => {
@@ -90,25 +73,9 @@ namespace PRMasterServer
             bool runMasterServer = true;
             bool runListServer = true;
             
-
-            
-            //string supportedgamename = "civ4bts";
-                        
-                        LogGreen("Use /help command to get server's attention");
+            LogGreen("Use /help command to get server's attention");
 
             IPAddress bind = IPAddress.Any;
-
-            //Params rewrtitten
-          //  LoginDatabase.Initialize("logindb.db3", log, logError);
-           // gameName = "civ4bts";
-           // string argsfake = "master,login,cdkey,list,natneg";
-           // List<string> serverTypes = argsfake.Split(char.Parse(",")).Select(s => { return s.Trim().ToLower(); }).ToList();
-            //runLoginServer = serverTypes.IndexOf("login") >= 0;
-            //runNatNegServer = serverTypes.IndexOf("natneg") >= 0;
-            //runListServer = serverTypes.IndexOf("list") >= 0;
-           // runMasterServer = serverTypes.IndexOf("master") >= 0;
-            //runCdKeyServer = serverTypes.IndexOf("cdkey") >= 0;
-
 
              if (args.Length >= 1)
              {
@@ -137,7 +104,7 @@ namespace PRMasterServer
                          if ((i >= args.Length - 1))
                          {
                              //gameName = "civ4bts";
-                             Log("supported games: civ4, civ4bts, civ4btsjp");
+                             //Log("supported games: civ4, civ4bts, civ4btsjp");
                          }
                          else
                          {
@@ -198,43 +165,11 @@ namespace PRMasterServer
                 string s = Console.ReadLine();
                 if (s == "/help") 
                 { 
-                    LogBlue("kay, dis some help for ye:"); 
-                    LogBlue("/help - to get help;"); 
-                    LogBlue("/sendF27900 X - send from (supposedly) SB, where X are hex symbols separated with commas to be sent, example: /sendF27900 0x00,0x06,0x06,0x06");
-                    LogBlue("/list - to see current serverlist;");
-                
-                
+                    LogBlue("type /list - to see current serverlist;");
                 } else
-                    if (s == "/sendF27900 X") 
-                {
-                    LogBlue("DIS IS AN ERORR AGAIN NOOB");
-                       // PRMasterServer.Servers.ServerListRepor
-                    try {
-                        //PRMasterServer.Servers.
-                      //  SendToClient();
-                    }
-                    catch (Exception ex)
-                    {
-                        Log("DIS IS AN ERORR AGAIN NOOB"+ ex.ToString());
-                    }
-
-                    } else
-                        if (s=="soplikita")//(s.Substring(0,3) == "/sa")//sa")
-                        {//8
-                            LogBlue("/SA");
-                            LogBlue(s);
-                            string arg1=s.Substring(4,8);
-                            LogBlue(arg1);
-                            
-                            PRMasterServer.Servers.ServerListRetrieve.SendToAllConnected(arg1);
-                            
-                            //string v;
-                           // Console.WriteLine(PRMasterServer.Servers["hostname"]);
-                            //ServerListReport.Servers.TryGetValue("hostname", out v);
-                           // LogBlue(v);//"testsaddasads"); 
-                        }//isSupported("hh"); } 
-                        else
-                            if (s == "/list") { PRMasterServer.Servers.ServerListRetrieve.printSLinfo(); }
+                 if (s == "/list") { PRMasterServer.Servers.ServerListRetrieve.printSLinfo(); }
+                            else
+                     if (s == "/decsht") { PRMasterServer.Servers.ServerListRetrieve.DecodeShit(); }//testing some stuff
                         
 
 				Thread.Sleep(500);
@@ -275,7 +210,6 @@ namespace PRMasterServer
             ConsoleColor c = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Magenta;
             Console.WriteLine(String.Format("[{0}] {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture), message));
-            //Console.ForegroundColor = ;
             Console.ForegroundColor = ConsoleColor.Gray;
         }
         //LOGS FROM ServerListRetrieve
@@ -284,16 +218,13 @@ namespace PRMasterServer
             ConsoleColor c = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine(String.Format("[{0}] {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture), message));
-            //Console.ForegroundColor = ;
             Console.ForegroundColor = ConsoleColor.Gray;
         }
         public static void LogControl(string message)
         {
-            //ConsoleColor c = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.White;
             Console.WriteLine(String.Format("[{0}] {1}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff", CultureInfo.InvariantCulture), message));
-            //Console.ForegroundColor = ;
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.BackgroundColor = ConsoleColor.Black;
             Console.WriteLine(" ");
