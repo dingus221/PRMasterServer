8d7165f10469fc08aa8ba299d9847132f74bc099
Zulan <github@zulan.net>
2015-03-22 00:01:44 +0100

    Refactoring, cleanup

--- SBQRC4/SBQRC4.py
+++ SBQRC4/SBQRC4.py
@@ -188,7 +188,7 @@ class SBQRServer:
         addr = ('.'.join(str(random.randrange(0, 256)) for _ in range(4)), 6500)
         self.hosts[addr] = GameHost(*addr)
         zz1 = """\x03\x79\x91\x42\x05\x6C\x6F\x63\x61\x6C\x69\x70\x30\x00\x31\x30\x39\x2E\x32\x30\x30\x2E\x31\x34\x37\x2E\x32\x00\x6C\x6F\x63\x61\x6C\x69\x70\x31\x00\x31\x30\x2E\x32\x34\x30\x2E\x31\x32\x2E\x32\x32\x00\x6C\x6F\x63\x61\x6C\x69\x70\x32\x00\x31\x39\x32\x2E\x31\x36\x38\x2E\x31\x33\x37\x2E\x31\x00\x6C\x6F\x63\x61\x6C\x70\x6F\x72\x74\x00\x36\x35\x30\x30\x00\x6E\x61\x74\x6E\x65\x67\x00\x31\x00\x73\x74\x61\x74\x65\x63\x68\x61\x6E\x67\x65\x64\x00\x33\x00\x67\x61\x6D\x65\x6E\x61\x6D\x65\x00\x63\x69\x76\x34\x00\x70\x75\x62\x6C\x69\x63\x69\x70\x00\x30\x00\x70\x75\x62\x6C\x69\x63\x70\x6F\x72\x74\x00\x30\x00\x68\x6F\x73\x74\x6E\x61\x6D\x65\x00\x74\x65\x61\x6D\x65\x72\x78\x70\x00\x67\x61\x6D\x65\x6D\x6F\x64\x65\x00\x6F\x70\x65\x6E\x73\x74\x61\x67\x69\x6E\x67\x00\x6E\x75\x6D\x70\x6C\x61\x79\x65\x72\x73\x00\x31\x00\x6D\x61\x78\x70\x6C\x61\x79\x65\x72\x73\x00\x33\x32\x00\x68\x6F\x73\x74\x6E\x61\x6D\x65\x00\x74\x65\x61\x6D\x65\x72\x78\x70\x00\x67\x61\x6D\x65\x76\x65\x72\x00\x30\x00\x70\x61\x73\x73\x77\x64\x00\x30\x00\x68\x6F\x73\x74\x70\x6F\x72\x74\x00\x32\x30\x35\x36\x00\x73\x74\x61\x67\x69\x6E\x67\x00\x31\x00\x6E\x65\x77\x67\x61\x6D\x65\x00\x31\x00\x6D\x61\x70\x6E\x61\x6D\x65\x00\x42\x61\x6C\x61\x6E\x63\x65\x64\x00\x67\x61\x6D\x65\x74\x79\x70\x65\x00\x00\x6D\x79\x6E\x75\x6D\x70\x6C\x61\x79\x65\x72\x73\x00\x31\x00\x6D\x61\x78\x6E\x75\x6D\x70\x6C\x61\x79\x65\x72\x73\x00\x33\x00\x6E\x75\x6D\x6D\x69\x73\x73\x69\x6E\x67\x00\x32\x00\x70\x69\x74\x62\x6F\x73\x73\x00\x30\x00\x00\x00\x01\x70\x6C\x61\x79\x65\x72\x5F\x00\x70\x69\x6E\x67\x5F\x00\x70\x69\x6E\x67\x5F\x00\x00\x64\x69\x6E\x67\x75\x73\x32\x32\x31\x2D\x74\x6B\x00\x30\x00\x30\x00\x00\x00\x00"""
-        self.hosts[addr].data = self.qr_parse03(zz1)
+        self.hosts[addr].data = SBQRServer.qr_parse03(zz1)
         self.hosts[addr].data['hostname'] = hostname
         self.hosts[addr].data['mapname'] = mapname
         self.hosts[addr].data['mynumplayers'] = nump
@@ -218,11 +218,12 @@ class SBQRServer:
                 resp += '\x00' * 4
             resp += ''.join(chr(random.randrange(0, 256)) for _ in range(4))  #random cookie here
             resp += rawdata[9:]
-            self.qrsendto(resp, (ip, port), 'qrforwto')
+            self.qr_send_to(resp, (ip, port), 'qrforwto')
         else:
             print('wrong data to forward')
 
-    def qr_parse03(self, raw):
+    @staticmethod
+    def qr_parse03(raw):
         r = {}
         cur = True  # true - key, false - value
         curstr = ''
@@ -230,7 +231,7 @@ class SBQRServer:
         prev00 = -2
         for y, x in enumerate(raw[5:]):
             if x == '\x00':
-                if prev00 + 1 == y and cur == True:  #if second \x00 in row and its where key should be, stop
+                if prev00 + 1 == y and cur:  #if second \x00 in row and its where key should be, stop
                     break
                 else:
                     if not cur:
@@ -244,7 +245,7 @@ class SBQRServer:
                 curstr += x
         return r
 
-    def qrsendto(self, resp, address, location):
+    def qr_send_to(self, resp, address, location):
         #sending from qr, with error handliing
         try:
             self.qr_socket.sendto(resp, address)
@@ -253,23 +254,21 @@ class SBQRServer:
             str(location) + str(x)
 
     def process_qr(self, recv_data, address):
-        print
-        'handle qr'
+        print('handle qr')
         if recv_data[0] == '\x09' and len(recv_data) >= 5:  #09,4xUid,'civ4bts','0'  - game checks if qr is up
             resp = '\xfe\xfd\x09' + recv_data[1:5] + '\x00'
-            self.qrsendto(resp, address, '09')
+            self.qr_send_to(resp, address, '09')
         elif recv_data[0] == '\x08' and len(recv_data) >= 5:  #08 4xuid - ping
             resp = '\xfe\xfd\x08' + recv_data[1:5]
-            self.qrsendto(resp, address, '08')
+            self.qr_send_to(resp, address, '08')
         elif recv_data[0] == '\x07' and len(recv_data) >= 5:  #06 ACK - no response
             hexprint(recv_data)
         elif recv_data[0] == '\x01' and len(recv_data) >= 5:  #resp to our challenge
             resp = '\xfe\xfd\x0a' + recv_data[1:5]
-            self.qrsendto(resp, address, '01')
+            self.qr_send_to(resp, address, '01')
         elif recv_data[0] == '\x03' and len(recv_data) >= 5:
-            print
-            '03'
-            parsed = self.qr_parse03(recv_data)
+            print('03')
+            parsed = SBQRServer.qr_parse03(recv_data)
             if parsed.get('statechanged', 0) == '3':
                 if address in self.hosts:
                     del self.hosts[address]
@@ -277,7 +276,7 @@ class SBQRServer:
                 self.hosts[address].sessionid = recv_data[1:5]
                 self.hosts[address].data = parsed
                 resp = '\xfe\xfd\x01' + recv_data[1:5] + ghchal
-                self.qrsendto(resp, address, '03-3')
+                self.qr_send_to(resp, address, '03-3')
                 self.sb_sendpush02(self.hosts[address])
             elif parsed.get('statechanged', 0) == '2':
                 if address in self.hosts:
@@ -315,10 +314,10 @@ class SBQRServer:
                 lips.append(value1)
         for x1 in random.choice(lips).split('.'):
             flags_buffer += chr(int(x1))
-            print
-            chr(int(x1))
-        flags_buffer += chr(int(host.data.get('localport', 6500)) / 256)
-        flags_buffer += chr(int(host.data.get('localport', 6500)) % 256)
+            print(chr(int(x1)))
+        port = int(host.data.get('localport', 6500))
+        flags_buffer += chr(port / 256)
+        flags_buffer += chr(port % 256)
         msg += flags_buffer
         for field in defaultfields:
             msg += host.data[field] + '\x00'
