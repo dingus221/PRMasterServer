1a8788d745c14c75bc34aaab6463479545d9d17d
Zulan <github@zulan.net>
2015-03-21 22:41:21 +0100

    - Format

--- SBQRC4/enc.py
+++ SBQRC4/enc.py
@@ -1,42 +1,43 @@
-#Based on code copied or translated from works: GsOpenSDK, ALuigi's projects, dwc_network_server_emulator, possibly other sources
+# Based on code copied or translated from works: GsOpenSDK, ALuigi's projects, dwc_network_server_emulator, possibly other sources
 
 #6 functions related to SB Crypting
 SCHALLCONST = "\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01\x01"
-class GOACryptState:
 
+
+class GOACryptState:
     def __init__(self):
         self.cards = bytearray(256)
-        for i in xrange(0,256):
-            self.cards[i]=i
-        self.rotor, self.ratchet, self.avalanche, self.last_plain, self.last_cipher = (int),(int),(int),(int),(int)
+        for i in xrange(0, 256):
+            self.cards[i] = i
+        self.rotor, self.ratchet, self.avalanche, self.last_plain, self.last_cipher = (int), (int), (int), (int), (int)
 
     #1/5
-    def keyrand(self,limit,mask,user_key,keysize,rsum,keypos):
+    def keyrand(self, limit, mask, user_key, keysize, rsum, keypos):
         if limit == 0:
-            return 0,rsum,keypos
+            return 0, rsum, keypos
         retry_limiter = 0
         while True:
-            rsum = (self.cards[rsum]+user_key[keypos]) % 256
+            rsum = (self.cards[rsum] + user_key[keypos]) % 256
             keypos += 1
             keypos %= 256
-            if keypos >=keysize:
+            if keypos >= keysize:
                 keypos = 0
                 rsum = (rsum + keysize) % 256
             u = mask & rsum
-            if retry_limiter>11:
+            if retry_limiter > 11:
                 u %= limit
             retry_limiter += 1
-            if u<=limit:
+            if u <= limit:
                 break
-        return u,rsum,keypos
+        return u, rsum, keypos
 
     #2/5
-    def GOACryptPreStart(self,key):
+    def GOACryptPreStart(self, key):
         rsum = 0
         keypos = 0
         mask = 255
-        for i2c in xrange(255,0,-1):#255-1
-            toswap,rsum,keypos = self.keyrand(i2c, mask, key, 8, rsum, keypos)                         
+        for i2c in xrange(255, 0, -1):  #255-1
+            toswap, rsum, keypos = self.keyrand(i2c, mask, key, 8, rsum, keypos)
             self.cards[i2c], self.cards[toswap] = self.cards[toswap], self.cards[i2c]
             if (i2c & (i2c - 1) ) == 0:
                 mask >>= 1
@@ -49,7 +50,7 @@ class GOACryptState:
         keypos = 0
 
     #3/5
-    def SBCryptStart(self,qfromkey,cchal,schal):
+    def SBCryptStart(self, qfromkey, cchal, schal):
         sklen = len(qfromkey)
         thekey = cchal
         for i in xrange(0, len(schal)):
@@ -58,7 +59,7 @@ class GOACryptState:
         return thekey
 
     #4/5
-    def GOAEncrypt(self,bp):
+    def GOAEncrypt(self, bp):
         rotor = self.rotor;
         ratchet = self.ratchet;
         avalanche = self.avalanche;
@@ -78,7 +79,8 @@ class GOACryptState:
             avalanche %= 256
             last_cipher = ( bp[i] ^
                             self.cards[(self.cards[avalanche] + swaptemp) & 255] ^
-                            self.cards[ self.cards[ (self.cards[last_plain] + self.cards[last_cipher] + self.cards[ratchet]) & 255 ] ]
+                            self.cards[self.cards[
+                                (self.cards[last_plain] + self.cards[last_cipher] + self.cards[ratchet]) & 255]]
                             ) % 256
             last_plain = bp[i]
             bp[i] = last_cipher
@@ -90,7 +92,7 @@ class GOACryptState:
         return bp
 
     #5/5
-    def GOADecrypt(self,bp):
+    def GOADecrypt(self, bp):
         rotor = self.rotor;
         ratchet = self.ratchet;
         avalanche = self.avalanche;
@@ -110,7 +112,8 @@ class GOACryptState:
             avalanche %= 256
             last_plain = ( bp[i] ^
                            self.cards[(self.cards[avalanche] + self.cards[rotor]) & 255] ^
-                           self.cards[ self.cards[ (self.cards[last_plain] + self.cards[last_cipher] + self.cards[ratchet]) & 255 ] ]
+                           self.cards[self.cards[
+                               (self.cards[last_plain] + self.cards[last_cipher] + self.cards[ratchet]) & 255]]
                            ) % 256
             last_cipher = bp[i]
             bp[i] = last_plain
@@ -121,13 +124,14 @@ class GOACryptState:
         self.last_cipher = last_cipher
         return bp
 
+
 #0/5
 def SBpreCryptHeader():
     data = ''
-    data += chr(2 ^ 236) #number of random bytes to follow xor \xEC
+    data += chr(2 ^ 236)  #number of random bytes to follow xor \xEC
     data += '\x01'
     data += '\x01'
-    data += chr(15 ^ 234) #challenge len(15) xor \xEA
+    data += chr(15 ^ 234)  #challenge len(15) xor \xEA
     data += SCHALLCONST
     return data
 
