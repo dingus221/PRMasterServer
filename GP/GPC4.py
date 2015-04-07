6d0bcf80f8073b0d926d43b297efec6a8dd8d321
Bruce Wayne <dingus221@users.noreply.github.com>
2015-04-07 10:56:51 +0300

    Update GPC4.py

--- GP/GPC4.py
+++ GP/GPC4.py
@@ -2,12 +2,13 @@
 #based on works: prmasterserver, miniircd, gsopensdk
 #
 #
-#ONE TCP SERVER SOCKET BASE     CHECK
+#ONE TCP SERVER SOCKET BASE                             CHECK
 #Session number bundled with socket instance
 #ONE DATABASE FOR USER INFORMATION
-#CHALLENGE DECODING PROCEDURE
-#PASSWORD ENCODING/DECODING, MD5-HASHING PROCEDURES
-##<|lc\1 <- (login or newuser) CHECK
+#PASSWORD GSBASE64DEC, GSENC, MD5-HASHING PROCEDURES
+#PASSWORD LOGINCHECK_TRANSFORMATION
+#PASSWORD -> PROOF TRANSFORMATION
+##<|lc\1 <- (login or newuser)                         CHECK
 ##>|login -> lc\2
 ##>|newuser -> nur
 ##<|bdy,blk,bm
@@ -19,6 +20,8 @@
 import socket
 import select
 import time
+import sqlite3
+from gs_consts2 import *
 
 
 class GPClient:
@@ -30,24 +33,56 @@ class GPClient:
         self.__readbuffer = ""
         self.__writebuffer = ""
         self.__sent_ping = False
+        self.session = 1#??? session number generator utility
 
     def write_queue_size(self):
         return len(self.__writebuffer)
 
-    def LOGIN(self,arg):
-        print "login" + str(arg)
+    def LOGIN(self,data):
+        print "login"
+        '''\login\\challenge\4jv99yxEnyNWrq6EUiBmsbUfrkgmYF4f\
+           uniquenick\EvilLurksInternet-tk\partnerid\0\
+           response\45f06fe0f350ae4e3cc1af9ffe258c93\
+           firewall\1\port\0\productid\11081\gamename\civ4bts\
+           namespaceid\17\sdkrevision\3\id\1\final\
+        '''
+        self.message("\\error\\\\err\\516\\fatal\\\\errmsg\\This account name is already in use!\\id\\1\\final\\")
 
-    def NEWUSER(self,arg):
-        print "NEWUSER" + str(arg)
+    def NEWUSER(self,data):
+        print "NEWUSER"
+        if (5<len(data.get('nick',''))<24 and  #checking len of nick
+            len(data.get('email',''))>2   and  #chkng len of email
+            len(data.get('passwordenc',''))>4):#chkng len of passwenc
+            #check if user exists
+            if self.server.db.db_chk_usr(data['nick']) == 0:
+                #prepare password
+                #store data
+                #send response
+                self.message("\\error\\\\err\\515\\fatal\\\\errmsg\\Derp error!\\id\\1\\final\\")
+            else:
+                #name exists
+                self.message("\\error\\\\err\\516\\fatal\\\\errmsg\\This account name is already in use!\\id\\1\\final\\")
+        else:
+            #wrong len
+            self.message("\\error\\\\err\\0\\fatal\\\\errmsg\\Error creating account, check length!\\id\\1\\final\\")
+        
+        #add data to db
+        #send response?
+        '''\newuser\\email\qqq@qq\nick\borf-tk\passwordenc\J8DHxh7t\
+            productid\11081\gamename\civ4bts\namespaceid\17\uniquenick\borf-tk\
+            partnerid\0\id\1\final\
+        '''
+        
 
-    def GETPROFILE(self,arg):
-        print "GETPROFILE" + str(arg)
+    def GETPROFILE(self,data):
+        print "GETPROFILE"
 
-    def STATUS(self,arg):
-        print "STATUS" + str(arg)
+    def STATUS(self,data):
+        print "STATUS"
 
-    def UNKNOWN(self,arg):
-        print "unknown" + str(arg)
+    def UNKNOWN(self,data):
+        print "unknown"
+        print data
         
         
 
@@ -63,13 +98,8 @@ class GPClient:
                'newuser':self.NEWUSER,
                'getprofile':self.GETPROFILE,
                'status':self.STATUS}
-        com.get(header[0], self.UNKNOWN)(header[1])
+        com.get(header[0], self.UNKNOWN)(prepared)
         
-        '''\login\\challenge\4jv99yxEnyNWrq6EUiBmsbUfrkgmYF4f\
-           uniquenick\EvilLurksInternet-tk\partnerid\0\
-           response\45f06fe0f350ae4e3cc1af9ffe258c93\
-           firewall\1\port\0\productid\11081\gamename\civ4bts\
-           namespaceid\17\sdkrevision\3\id\1\final\ '''
     
     def socket_readable_notification(self):
         try:
@@ -104,14 +134,28 @@ class GPClient:
     def check_aliveness(self):
         pass
 
+class DBobject:
+    def __init__(self,path):
+        self.dbcon = sqlite3.connect(path)
+        self.dbcur = self.dbcon.cursor()
+        self.dbcur.execute("create table if not exists users ( id INTEGER PRIMARY KEY, name TEXT NOT NULL, password TEXT NOT NULL, email TEXT NOT NULL, country TEXT NOT NULL, lastip TEXT NOT NULL, lasttime INTEGER NULL DEFAULT '0', session INTEGER NULL DEFAULT '0' );")
+        self.dbcon.commit()
+
+    def db_chk_usr(self, uname):#chk if user exists?
+        print "db_chk_usr"
+        self.dbcur.execute("SELECT EXISTS(SELECT name FROM users WHERE name='" + uname + "' LIMIT 1);")
+        return self.dbcur.fetchone()[0]
+    
+
 class GPServer:
     def __init__(self):
         self.GPClients = {} # Socket --> Client instance
 
     def remove_GPClient(self, GPClient, quitmsg): #cant delete clients inside their own functions?
         del self.GPClients[GPClient.socket]
-
+        
     def Start(self):
+        self.db = DBobject(dbpath)
         a4 = ("0.0.0.0",29900)
         self.gp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.gp.setblocking(0)
