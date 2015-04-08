249d1e5dcaf172071347da14ebb3d0aa3976d603
Bruce Wayne <dingus221@users.noreply.github.com>
2015-04-08 11:28:14 +0300

    corrected a bug related to max integer number
    Python just converts integer to long when its too big, and delphi (probably c too) subtracts max integer from the number.

--- GP/gsenc2.py
+++ GP/gsenc2.py
@@ -2,6 +2,8 @@
 #Sources: GSOpenSDK, aluigi's works
 import base64
 import hashlib
+from gs_utils import *
+
 
 def gsPWDecHash(pwenc):
     md5 = hashlib.md5()
@@ -9,19 +11,15 @@ def gsPWDecHash(pwenc):
     return md5.hexdigest()
 
 def gsPWDecodeFunc0(pwenc):
-    pwencx = base64.b64decode(pwenc)
+    pwencx = base64.b64decode(pwenc.replace('[', '+').replace(']', '/').replace('_', '='))
     return gsPWDecodeFunc1(pwencx)
 
 def gsPWDecodeFunc1(pwencx):
     num = 2037412711
     for i in xrange(0,len(pwencx)):
-        d = 255
-        if d != 0:
-            num = gsPWDecodeFunc2(num)
-            a = num % d
-        else:
-            a = 0
-        pwencx = pwencx[:i] + chr(ord(pwencx[i]) ^ (a % 256) ) + pwencx[i+1:]       
+        num = gsPWDecodeFunc2(num)
+        a = num % 255
+        pwencx = pwencx[:i] + chr(ord(pwencx[i]) ^ a ) + pwencx[i+1:]
     return pwencx
 
 def gsPWDecodeFunc2(num):
@@ -29,7 +27,9 @@ def gsPWDecodeFunc2(num):
     a = num & 65535
     c *= 16807
     a *= 16807
-    a += (c & 32767) << 16
+    a += ( (c & 32767) << 16 )
+    if a>=2147483648:
+        a = -2147483648 + (a - 2147483648)
     if a < 0:
         a &= 2147483647
         a += 1
