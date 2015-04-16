5f0f5e37f5fccf1addbd8a8c76e364108ec0e9c1
Zulan <github@zulan.net>
2015-04-16 21:57:58 +0200

    - pep8 cleanup

--- GP/gsenc2.py
+++ GP/gsenc2.py
@@ -9,9 +9,11 @@ def gsPWDecHash(pwenc):
     md5.update(gsPWDecodeFunc0(pwenc))
     return md5.hexdigest()
 
+
 def gsPWDecodeFunc0(pwenc):
     pwencx = base64.b64decode(pwenc.replace('[', '+').replace(']', '/').replace('_', '='))
     return gsPWDecodeFunc1(pwencx)
+
 
 def gsPWDecodeFunc1(pwencx):
     num = 2037412711
@@ -21,13 +23,14 @@ def gsPWDecodeFunc1(pwencx):
         pwencx = pwencx[:i] + chr(ord(pwencx[i]) ^ a ) + pwencx[i+1:]
     return pwencx
 
+
 def gsPWDecodeFunc2(num):
     c = (num >> 16) & 65535
     a = num & 65535
     c *= 16807
     a *= 16807
-    a += ( (c & 32767) << 16 )
-    if a>=2147483648:
+    a += ((c & 32767) << 16)
+    if a >= 2147483648:
         a += -4294967296L
     if a < 0:
         a &= 2147483647
@@ -38,12 +41,14 @@ def gsPWDecodeFunc2(num):
         a += 1
     return a
 
-def PW_Hash_to_Resp(pwhash,unick,schal,cchal):
+
+def PW_Hash_to_Resp(pwhash, unick, schal, cchal):
     md5 = hashlib.md5()
-    md5.update(pwhash + (' '*48) + unick + cchal + schal + pwhash)
+    md5.update(pwhash + (' ' * 48) + unick + cchal + schal + pwhash)
     return md5.hexdigest()
 
-def PW_Hash_to_Proof(pwhash,unick,schal,cchal):
+
+def PW_Hash_to_Proof(pwhash, unick, schal, cchal):
     md5 = hashlib.md5()
-    md5.update(pwhash + (' '*48) + unick + schal + cchal + pwhash)
+    md5.update(pwhash + (' ' * 48) + unick + schal + cchal + pwhash)
     return md5.hexdigest()
